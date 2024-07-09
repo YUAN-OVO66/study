7969a3063bcc639b277e5dee54aba0b83685e8ea
YUAN-OVO66 <1440556275@qq.com>
2024-07-10 00:13:53 +0800

    element组件

---
+++ 开发/javaweb/web前端/element/element-project/vite.config.js
@@ -0,0 +1,57 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import path from 'path'
+const timestamp = new Date().getTime()
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: '[name]',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+        entryFileNames: `js/${timestamp}.[hash].js`,
+        assetFileNames: `[ext]/${timestamp}.[hash].[ext]`,
+        chunkFileNames: () => {
+          return `js/${timestamp}.[hash].js`
+        }
+      }
+    }
+  }
+})
