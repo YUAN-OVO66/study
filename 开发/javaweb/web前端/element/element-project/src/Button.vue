d93c1e73ceee5fa4f56c048fe9e6b58e3524ddb2
YUAN-OVO66 <1440556275@qq.com>
2024-07-10 00:14:05 +0800

    组件

---
+++ 开发/javaweb/web前端/element/element-project/src/Button.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
+</script>
+
+<template>
+    <div class="mb-4">
+      
+      <el-button type="primary" disabled="true">编辑</el-button>
+      <el-button type="success" loading="true">查看</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+  
+    <div class="mb-4">
+      <el-button type="warning" plain>Warning</el-button>
+      <el-button type="danger" plain>Danger</el-button>
+    </div>
+  
+
+  </template>
+  
+
+  
